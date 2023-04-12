dc3b118fbffadf847fbe76bd78abeb04aeb076b6
Ezekiel A. Mitchell <ezekielem@outlook.com>
2023-04-12 06:03:17 -0700

    Update plant-watering-system.ino
    added code for printing moisture level onto liquid crystal lcd

--- plant-watering-system/plant-watering-system.ino
+++ plant-watering-system/plant-watering-system.ino
@@ -1,15 +1,22 @@
+#include <LiquidCrystal.h>
+
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+const int rs = 12, en = 11, d4 = 6, d5 = 5, d6 = 4, d7 = 3;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
 int IN1 = 2;
 int Pin1 = A0;
 float value1 = 0;
 
 void setup() {
   // put your setup code here, to run once:
+  lcd.begin(16, 2);
   Serial.begin(9600);
   pinMode(IN1, OUTPUT);
   pinMode(Pin1, INPUT);
   digitalWrite(IN1, HIGH);
   delay(500);
-
 }
 
 void loop() {
@@ -24,6 +31,10 @@ void loop() {
   }
 
   Serial.println();
+  lcd.print("MOISTURE LEVEL:");
+  lcd.setCursor(0, 2);
+  lcd.print(value1);
   delay(1000);
+  lcd.clear();
+  }
 
-}
